c4698e02e9e2cee43ae485baf85a9af3a3714a28
luciarodriguezl <166874555+luciarodriguezl@users.noreply.github.com>
2024-05-01 22:51:48 -0600

    Add files via upload

---
+++ avance/funciones.js
@@ -0,0 +1,32 @@
+var couter=1;
+setInterval(function(){
+    document.getElementById('radio' + counter) .checked=true;
+    counter++;
+    if(counter > 4){
+	    counter=1;
+    }
+},5000);
+window.addEventListener("load",function(){
+    document.getElementById('yo antes de ti').addEventListener("click",function()
+
+})
+window.addEventListener("load",function(){
+    document.getElementById('a dos metros de ti').addEventListener("click",function()
+
+})
+window.addEventListener("load",function(){
+    document.getElementById('bajo la misma estrella').addEventListener("click",function()
+
+})
+window.addEventListener("load",function(){
+    document.getElementById('tres metrs de ti').addEventListener("click",function()
+
+})
+window.addEventListener("load",function(){
+    document.getElementById('fabricante de lagrimas').addEventListener("click",function()
+
+})
+window.addEventListener("load",function(){
+    document.getElementById('corazon malherido').addEventListener("click",function()
+
+})
